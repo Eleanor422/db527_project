2d67414223b0efe2ec3bd3a3511a9be3146df368
Eleanor422 <sh1374@rutgers.edu>
2021-10-15 17:04:02 -0400

    added pagination

--- db527_web/views.py
+++ db527_web/views.py
@@ -3,13 +3,12 @@ from django.db import connections
 import time
 
 
-# SELECT * FROM PG_TABLE_DEF
-# SHOW TABLES
-
 def index(request):
     return render(request, 'index.html')
 
 def search(request):
+    # Get the selected db and user's query
+    # Return the query result or error message
     if request.GET.get('selectDatabase') == 'Redshift':
         db = 'redshift'
     else:
@@ -28,18 +27,22 @@ def perform_query(db,query):
             try:
                 cursor.execute(query)
 
+                elapsed_time = round((time.time() - start_time) * 1000, 3)
+
                 if cursor.description is None:
                     return cursor.rowcount
 
-                if cursor.rowcount > 20:
-                    rows = list(list(row) for row in cursor.fetchmany(20))
+                columns = [col[0] for col in cursor.description]
+                if cursor.rowcount > 100:
+                    rows = list(list(row) for row in cursor.fetchmany(100))
+                    message = 'We limit the number of rows displayed to 100.'
+                    result = {'message':message, 'columns': columns, 'rows': rows, 'elapsed_time': elapsed_time}
+                    return result
                 else:
                     rows = list(list(row) for row in cursor.fetchall())
+                    result = {'columns': columns, 'rows': rows, 'elapsed_time': elapsed_time}
+                    return result
 
-                columns = [col[0] for col in cursor.description]
-                elapsed_time = round((time.time() - start_time)*1000,3)
-                result = {'columns': columns, 'rows': rows, 'elapsed_time': elapsed_time}
-                return result
 
             except Exception as e:
                 elapsed_time = round((time.time() - start_time) * 1000, 3)
